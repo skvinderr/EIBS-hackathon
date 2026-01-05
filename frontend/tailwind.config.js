094dbb78803482ca7ae4e361fd8288db1f2e0ad6
skvinderr <skvinderr@gmail.com>
2026-01-05 22:22:16 +0530

    feat: Add Tailwind CSS and PostCSS configuration files.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     darkMode: ["class"],
     content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
     ],
     theme: {
-        extend: {
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)'
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
             },
-            colors: {}
-        }
+        },
+        extend: {},
     },
     plugins: [require("tailwindcss-animate")],
 }
